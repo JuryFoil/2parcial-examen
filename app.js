b08379b3157c5288417bf0613ada875f2aeb8e0e
JuryFoil <mgarcia14@ucol.mx>
2023-11-30 11:20:48 -0600

    examn

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
 let eventos=["Boda", "Cumpleaños", "XV años"];
 
-showDom("li", eventos);
+// showDom("li", eventos);
 
-function showDom(elemento, arr){
-    for(let i=0; i<arr.length; i++){
-        document.getElementById(elemento).innerHTML+=
-        "<li>"+arr[i]+"</li>";
-    }
-}
-
-
-// function crear() {
-//    
+// function showDom(element, arr){
+//     for(let i=0; i<arr.length; i++){
+//         document.getElementById(element).innerHTML+=
+//         "<li>"+arr[i]+"</li>";
+//     }
 // }
 
-function borrar() {
+function crear(){
+    let input=document.getElementById("inputt").value;
+    eventos=eventos.concat(input);
+    document.getElementById("inputt").value="";
+    document.getElementById("lis").innerHTML=eventos.join("<li>");
 }
+
+
+function borrar(){ 
+    console.log(eventos)
+    eventos.splice(-1,1);
+    document.getElementById("lis").innerHTML=eventos.join("<li>");
+}
+
+
